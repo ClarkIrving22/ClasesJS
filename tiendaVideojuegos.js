6741d8d017fb0f204582cb09b72b9a605c2bf067
ClarkIrving22 <clark.irving@icloud.com>
2021-05-04 01:11:17 -0500

    Creando clases y objetos

--- tiendaVideojuegos.js
+++ tiendaVideojuegos.js
@@ -1,1 +1,28 @@
-console.log("Hola")
+class juego{
+    constructor(nombre, precio, genero, clasificacion){
+        this.nombre = nombre
+        this.precio = precio
+        this.genero = genero
+        this.clasificacion = clasificacion
+    }
+
+    mostrar(){
+        console.log(this.nombre, this.precio)
+    }
+}
+
+class tienda{
+    constructor(juegos){
+        this.juegos = juegos
+    }
+}
+
+const rainbow = new juego('Rainbow Six Siege', 49, 'FPS', '+18')
+const rocket = new juego('Rocket League', 23, 'Deportes', '+13')
+const csgo = new juego('Counter Strike GO', 0, 'FPS', '+18')
+
+const tiendaNorte = new tienda([rainbow, rocket, csgo])
+
+for(let i of tiendaNorte.juegos){
+    console.log(i.mostrar())
+}
