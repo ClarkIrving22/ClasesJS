bd9111659ae1874cadfa7858fab4a2fd3f274319
ClarkIrving22 <clark.irving@icloud.com>
2021-05-15 16:12:04 -0500

    Objetos instanciados

--- mezclaObjetos.js
+++ mezclaObjetos.js
@@ -3,6 +3,10 @@ class usuario{
         this.id = id;
         this.nombre = nombre;
     }
+    
+    mostrar(){
+        console.log(this.id, this.nombre);
+    }
 }
 
 class producto{
@@ -11,6 +15,24 @@ class producto{
         this.nombre = nombre;
         this.idusuario = idusuario;
     }
+
+    mostrar(){
+        console.log(this.id, this.nombre, this.idusuario)
+    }
 }
 
 const usuarios = [];
+usuarios.push(new usuario(1, 'Clark'));
+usuarios.push(new usuario(2, 'Diego'));
+usuarios.push(new usuario(3, 'Kronner'));
+usuarios.push(new usuario(4, 'Waffles'));
+usuarios.push(new usuario(5, 'Fara'));
+
+usuarios.map((usuario) => {
+    usuario.mostrar();
+})
+
+const productos = [];
+productos.push(new producto(1, 'Rainbow Six', 3));
+productos.push(new producto(2, 'GTA V', 1));
+productos.push(new producto(3, 'Fortnite', 4));
